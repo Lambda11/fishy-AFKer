e4c4cc61fd7f147c9310522f19155a877edc220f
Lambda <5417634+Lambda11@users.noreply.github.com>
2019-06-16 16:34:39 +0300

    works now

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports = function fishy-AFKer(mod) {
+module.exports = function fishy_AFKer(mod) {
 	mod.hook('C_UPDATE_AFK_STATUS', 1, event=>{
 		mod.command.message("AFK: " + event.status)
 		return false;
